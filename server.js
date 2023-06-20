ce5c4668b713a72c889776bfa18b1a8575ae312f
joao12021996 <30709396+joao12021996@users.noreply.github.com>
2023-06-20 13:50:49 +0200

    SPOT-82 lol back to http as nginx will do it for me

--- server.js
+++ server.js
@@ -1,44 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
-const https = require("https");
-const fs = require("fs");
-const bodyParser = require("body-parser");
-const cors = require('cors');
 
 // App
 const app = express();
 
+// Set port
+const port = process.env.PORT || "1337";
+app.set("port", port);
+
+const cors = require('cors');
 app.use(cors({
     origin: '*'
 }));
-
-// Post request for geetting input from
-// the form
-app.post("/mssg", function (req, res) {
-  // Logging the form body
-  console.log(req.body);
-    
-  // Redirecting to the root
-  res.redirect("/");
-});
-
 app.use('/', routes);
 
-// Configuring express to use body-parser
-// as middle-ware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Creating object of key and certificate
-// for SSL
-const options = {
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
-};
-
-// Creating https server by passing
-// options and app object
-https.createServer(options, app)
-  .listen(1337, function (req, res) {
-  console.log("Server started at port 1337");
-});
+// Server
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
