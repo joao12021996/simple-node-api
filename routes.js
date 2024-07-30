98233685e7cb21bf414a1aa4a89da9e258d93f21
Joao Goncalves <andre12021996@gmail.com>
2024-07-29 19:35:56 -0500

    SPOT-104 Add global stats methods

--- routes.js
+++ routes.js
@@ -2,12 +2,46 @@ const express = require('express');
 const wait = require("./json/wait");
 const ok = require("./json/ok");
 const fns = require('date-fns');
+const fs = require('fs') 
 
 var userWait = new Map()
 const users = new Map()
 const userInfo = new Map()
 const customUsers = new Map()
 const userGame = new Map()
+
+function replacer(key, value) {
+  if(value instanceof Map) {
+    return {
+      dataType: 'Map',
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  } else {
+    return value;
+  }
+}
+function reviver(key, value) {
+  if(typeof value === 'object' && value !== null) {
+    if (value.dataType === 'Map') {
+      return new Map(value.value);
+    }
+  }
+  return value;
+}
+
+function initGlobalMap(){
+  const fs = require('fs'); 
+  try{ 
+    const data = fs.readFileSync('save.txt') 
+    return JSON.parse(data.toString(), reviver)
+  } catch (err) { 
+    console.error(err); 
+  } 
+
+  return new Map()
+}
+
+const globalGuesses = initGlobalMap()
 
 const router = express.Router();
 
@@ -16,18 +50,32 @@ setInterval(function(){
 }, 10000)
 //every 10 seconds
 
+setInterval(function(){
+  saveFile()
+}, 60 * 60 * 1000)//60 * 60 * 1000
+//every hour
+
 function countPlayers(){
-  total = 0
-  for (let [_, value] of userWait.entries()) {
-    total = total + value.size
+  if(users === undefined){
+    return 0
   }
 
-  return total
+  console.log(users.entries)
+
+  return users.size
+}
+
+function saveFile(){
+  const fs = require('fs'); 
+  fs.writeFile('save.txt', JSON.stringify(globalGuesses, replacer), (err) => { 
+    if (err) throw err; 
+    console.log('File written!'); 
+  }); 
 }
 
 router.get("/newgame", function(req, res) { 
   console.log('newgame')
-  console.log(userWait)
+  console.log(userWait.entries())
 
   const currentUser = req.query.name
   const currentUserCustom = req.query.customname
@@ -96,9 +144,11 @@ router.get("/newgame", function(req, res) {
       users.set(user, currentUser)
       users.set(currentUser, user)
 
+      console.log(users)
+
       const date = fns.startOfDay(new Date(Math.floor((Math.random() * 2) * Date.now())))
       const gameMap = new Map() 
-      gameMap.set('gameDate', date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDay())
+      gameMap.set('gameDate', date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate())
       gameMap.set('guesses', [])
       gameMap.set('guessesPlayer1', [])
       gameMap.set('guessesPlayer2', [])
@@ -126,7 +176,7 @@ router.get("/newgame", function(req, res) {
     }
   }
 
-  res.json(wait);
+  return res.json(wait);
 });
 
 router.get("/addguess", function(req, res) {
@@ -148,14 +198,112 @@ router.get("/addguess", function(req, res) {
 
   console.log(guess)
 
-  res.json(ok);
+  return res.json(ok);
 });
+
+function getKey(key) {
+  var localKey = key
+  if(localKey === null || localKey  === undefined) {
+    localKey  = ''
+  }
+
+  return localKey
+}
+
+router.get("/addglobalguess", function(req, res) {
+  console.log('addglobalguess')
+  console.log(req.query.guess)
+
+  const guess = req.query.guess
+  const timestamp = new Date(+req.query.year, +req.query.month, +req.query.day).getTime()
+  const guessIndex = +req.query.guessindex 
+  const key = getKey(req.query.key)
+  const timestampKey = key + timestamp
+
+  if(globalGuesses.get(timestampKey) === undefined) {
+    globalGuesses.set(timestampKey, new Map())
+  }
+
+  const timestampMap = globalGuesses.get(timestampKey)
+
+  if(timestampMap.get(guessIndex) === undefined) {
+    timestampMap.set(guessIndex, new Map())
+  }
+
+  const guessIndexMap = timestampMap.get(guessIndex)
+  if(guessIndexMap.get(guess) === undefined) {
+    guessIndexMap.set(guess, 1)
+  } else {
+    guessIndexMap.set(guess, guessIndexMap.get(guess) + 1)
+  }
+
+  return res.json(ok);
+});
+
+router.get("/getglobalguesses", function(req, res) {
+  console.log('getglobalguesses')
+
+  const timestamp = new Date(+req.query.year, +req.query.month, +req.query.day).getTime()
+  //not transforming into a number here (done below), because I can't, for some reason, === on NaN
+  const guessIndex = req.query.guessindex 
+  const key = getKey(req.query.key)
+  const timestampKey = key + timestamp
+
+  console.log(timestampKey)
+  console.log(guessIndex)
+  console.log(globalGuesses)
+
+  if(globalGuesses.get(timestampKey) === undefined) {
+    console.log("time undefined")
+    return res.json([
+      { 
+        global: ""
+      }
+    ])
+  }
+
+  const timestampMap = globalGuesses.get(timestampKey)
+  
+  if(guessIndex === undefined) {
+    console.log("guessIndex undefined")
+    return res.json([
+      { 
+        global: JSON.stringify(timestampMap, replacer)
+      }
+    ])
+  }
+
+  var guessIndexMap = timestampMap.get(+guessIndex)
+  if(guessIndexMap === undefined) {
+    console.log("guessIndexMap undefined")
+    return res.json([
+      { 
+        global: ""
+      }
+    ])
+  }
+
+  return res.json([
+    { 
+      global: JSON.stringify(guessIndexMap, replacer)
+    }
+  ])
+
+})
+
+router.get("/saveGlobalGuesses", function(req, res) {
+  console.log('saveGlobalGuesses')
+
+  saveFile()
+
+  return res.json(ok);
+})
 
 router.get("/getguess", function(req, res) {
   console.log('getguess')
 
   if(userGame.get(req.query.name) === undefined){
-    res.json([
+    return res.json([
       {
         response: '404'
       }
@@ -163,7 +311,7 @@ router.get("/getguess", function(req, res) {
   }
 
   console.log(userGame.get(req.query.name))
-  res.json([
+  return res.json([
     {
       response: 'ok',
       guesses: userGame.get(req.query.name).get('guesses'),
@@ -175,35 +323,11 @@ router.get("/getguess", function(req, res) {
   ])
 });
 
-router.get("/onlineplayers", function(req, res){
+router.get("/onlineplayers", function(_, res){
   console.log('onlineplayers')
-
-  const gameMode = req.query.gamemode
-  const hardMode = req.query.hardmode
-  const gridHardMode = req.query.gridhardmode
-  const hiddenLetterMode = req.query.hiddenlettermode
-  const language = req.query.language
-
-  var key = ''
-  if(gameMode){
-    key = 'team'
-  }
-  if(hardMode){
-    key = key + 'hardMode'
-  }
-  if(gridHardMode){
-    key = key + 'gridHardMode'
-  }
-  if(hiddenLetterMode){
-    key = key + 'hiddenLetterMode'
-  }
-  if(language){
-    key = key + language
-  }
 
   return res.json([
     {
-      subTotal: userWait.get(key).size(),
       total: countPlayers()
     }
   ])
@@ -249,11 +373,14 @@ router.get("/endgame", function(req, res) {
   users.delete(currentUser)
   userGame.delete(currentUser)
   userInfo.delete(currentUser)
-  userWait.get(key).pop(currentUser)
   users.delete(opponent)
   userGame.delete(opponent)
   userInfo.delete(opponent)
-  userWait.get(key).pop(opponent)
+
+  if(userWait.get(key) === undefined){
+    userWait.get(key).pop(currentUser)
+    userWait.get(key).pop(opponent)
+  }
 
   return res.json([
     { 
